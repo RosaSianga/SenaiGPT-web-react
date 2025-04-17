fe75cda9368139db88fa267bea5ef9bda06794c5
Rosa Sianga <rosa.sianga@gmail.com>
2025-04-16 21:50:39 -0300

    Aula 16-04-25

--- senai-gtp-web-react/src/pages/chat/index.jsx
+++ senai-gtp-web-react/src/pages/chat/index.jsx
@@ -1,3 +1,18 @@
+import ".chat.css";
+import chat from "../../assets/imgs/chat.svg";
+import lixeira from "../../assets/imgs/lixeira.svg";
+import sol from "../../assets/imgs/sol.svg";
+import user from "../../assets/imgs/user.svg";
+import setaCima from "../../assets/imgs/seta_cima.svg";
+import setaLado from "../../assets/imgs/setaLado.svg";
+import chatGPT from "../../assets/imgs/ChatGPT";
+import chatVazio from "../../assets/imgs/chat_vazio.svg";
+import estrela from "../../assets/imgs/estrela.svg";
+import escudo from "../../assets/imgs/escudo.svg";
+import microfone from "../../assets/imgs/microfone.svg";
+import image from "../../assets/imgs/image.svg";
+import seta from "../../assets/imgs/seta.svg";
+
 
 function Chat() {
 
@@ -13,18 +28,18 @@ function Chat() {
                         <button class="btnchat" type="button"> + New chat </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/chat.svg" alt="Imagem chat"/>
-                                AI Chat Tool Ethics
+                            <img src={chat} alt="Imagem chat" />
+                            AI Chat Tool Ethics
                         </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/chat.svg" alt="Imagem chat"/>
-                                AI Chat Tool Impact Writing
+                            <img src={chat} alt="Imagem chat" />
+                            AI Chat Tool Impact Writing
                         </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/chat.svg" alt="Imagem chat"/>
-                                New chat
+                            <img src={chat} alt="Imagem chat" />
+                            New chat
                         </button>
 
                     </div>
@@ -33,28 +48,28 @@ function Chat() {
                     <div class="inferior">
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/lixeira.svg" alt="Imagem lixeira"/>
-                                Clear conversation
+                            <img src={lixeira} alt="Imagem lixeira" />
+                            Clear conversation
                         </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/sol.svg" alt="Imagem ligth mode"/>
-                                Ligth mode
+                            <img src={sol} alt="Imagem ligth mode" />
+                            Ligth mode
                         </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/User.svg" alt="Imagem My account"/>
-                                My account
+                            <img src={user} alt="Imagem My account" />
+                            My account
                         </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/seta_cima.svg" alt="Imagem update e FAQ"/>
-                                Updates & FAQ
+                            <img src={setaCima} alt="Imagem update e FAQ" />
+                            Updates & FAQ
                         </button>
 
                         <button class="botoes" type="button">
-                            <img src="../assets/imgs/seta_lado.svg" alt="Imagem logout"/>
-                                Log out
+                            <img src={setaLado} alt="Imagem logout" />
+                            Log out
                         </button>
 
                     </div>
@@ -66,67 +81,67 @@ function Chat() {
 
                     <div class="principal">
 
-                        <img class="imagem" src="../assets/imgs/ChatGPT.png" alt="Logo SenaiGPT"/>
+                        <img class="imagem" src={chatGPT} alt="Logo SenaiGPT" />
 
 
-                            <div class="tabela">
+                        <div class="tabela">
 
-                                <div class="btngrupo">
-                                    <h1>
-                                        <img src="../assets/imgs/chat_vazio.svg" alt="Imagem chat"/>
-                                            Examples
-                                    </h1>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                </div>
-
-
-                                <div class="btngrupo">
-                                    <h1>
-                                        <img src="../assets/imgs/estrela.svg" alt="Imagem chat"/>
-                                            Capalities
-                                    </h1>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                </div>
-
-
-                                <div class="btngrupo">
-                                    <h1>
-                                        <img src="../assets/imgs/escudo.svg" alt="Imagem chat"/>
-                                            Limitions
-                                    </h1>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                    <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
-                                </div>
-
+                            <div class="btngrupo">
+                                <h1>
+                                    <img src={chatVazio} alt="Imagem chat" />
+                                    Examples
+                                </h1>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
                             </div>
 
 
-                            <div class="pesquisa">
-
-                                <img src="../assets/imgs/microfone.svg" alt="Imagem microfone"/>
-                                    <img src="../assets/imgs/image.svg" alt="Imagem foto"/>
-
-                                        <input class="input" type="text" placeholder="Type message"/>
-
-                                            <img src="../assets/imgs/seta.svg" alt="Imagem foto"/>
-
-                                            </div>
-
-                                        </div>
-
-
-                                    </main>
-
+                            <div class="btngrupo">
+                                <h1>
+                                    <img src={estrela} alt="Imagem chat" />
+                                    Capalities
+                                </h1>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
                             </div>
 
 
-                        </>
-                        )
+                            <div class="btngrupo">
+                                <h1>
+                                    <img src={escudo} alt="Imagem chat" />
+                                    Limitions
+                                </h1>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                                <button class="botao" type="button">Explain Quantum Computing insimple terms</button>
+                            </div>
+
+                        </div>
+
+
+                        <div class="pesquisa">
+
+                            <img src={microfone} alt="Imagem microfone" />
+                            <img src={image} alt="Imagem foto" />
+
+                            <input class="input" type="text" placeholder="Type message" />
+
+                            <img src={seta} alt="Imagem foto" />
+
+                        </div>
+
+                    </div>
+
+
+                </main>
+
+            </div>
+
+
+        </>
+    )
 }
 
-                        export default Chat;
+export default Chat;
--- senai-gtp-web-react/src/pages/login/index.jsx
+++ senai-gtp-web-react/src/pages/login/index.jsx
@@ -1,6 +1,29 @@
 import "./login.css";
+import logo from "../../assets/imgs/Chat.png";
+import { useState } from "react";
 
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onLoginClick = async () => {
+
+        let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+
+            headers: {
+                "content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+
+        console.log(response);
+
+    }
 
     return (
         <>
@@ -11,30 +34,18 @@ function Login() {
 
                 <div className="robo-image">
 
-                    {/* <img class="robo" src="../assets/imgs/Sidebar.png" alt="Robo">  */}
-
                 </div>
-
 
                 <div className="login-container">
 
-                    <img className="logo" src="../assets/imgs/Chat.png" alt="Logo do SenaiGPT" />
-
-                    {/* 
-                        <Utilizado quando tem arquivo CSS, classe e id 
-                        <h1 id="meutitulo" class="titulo">Login</h1> 
-
-                        <Utilizado apenas no html -->
-                        <h1 style="font-size: 76px;">Login</h1> 
-
-                        <Utilizando a formação no CSS pela tag  */}
+                    <img className="logo" src={logo} alt="Logo do SenaiGPT" />
 
                     <h1>Login</h1>
 
-                    <input className="inpt" type="email" placeholder="Insira o e-mail" />
-                    <input className="inpt" type="password" placeholder="Insira a senha" />
+                    <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="Insira o e-mail" />
+                    <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="Insira a senha" />
 
-                    <button className="btn">Entrar</button>
+                    <button className="btn" onClick={() => onLoginClick()}>Entrar</button>
 
                 </div>
 
