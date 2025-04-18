20b16b213441c274a359bf8ec5e3a1215cf4ac8e
Rosa Sianga <rosa.sianga@gmail.com>
2025-04-17 21:45:04 -0300

    Criando rota entre as telas

--- senai-gtp-web-react/src/App.jsx
+++ senai-gtp-web-react/src/App.jsx
@@ -1,16 +1,28 @@
 import Chat from "./pages/chat";
 import Login from "./pages/login";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
 
-      <Login/>
-      <Chat/>
+      {/* <Login/>
+      <Chat/> */}
+
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Chat" element={<Chat />} />
+        </Routes>
+      </Router>
 
     </>
   )
 }
 
 export default App;
+
+
